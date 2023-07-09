1383b17a4495b88575495ab9e1ad622193cd0be3
Md Asjad <mdasjad895@gmail.com>
2023-07-09 20:41:14 +0530

    cleaned

--- authentication/middleware/auth.js
+++ authentication/middleware/auth.js
@@ -1,11 +1,12 @@
 const user = require("../models/register");
-
+//this auth is for if any one click any private link it will check whether user is verified or not if 
+//verified then allow else redirect login
 const auth = async (req, res, next) => {
   try {
     const username = req.cookies['user']; // Assuming the username is stored in the cookie as "user"
     console.log("auth:  "+username);
     
-    // Check if the user exists in the session or not
+    // check if the user exists in the session or not
     if (!username) {
       console.log("chor hai");
       // req.session.destroy(); Destroy the session
